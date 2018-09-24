16763c5c7f32a4611889ead0bb85165c4d60058a
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-09-24 15:20:45 -0700

    testing

--- routes/brands/single.js
+++ routes/brands/single.js
@@ -1,8 +1,11 @@
 const data = require('../../brands.json');
 
 module.exports = (req, res) => {
+
   const path = req.params.path;
   const brand = data.brands.find(m => m.path === path);
+
+
 
   res.status(200).json({ brand });
 };
