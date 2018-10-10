329f6d47953642f1154e472babae06910e49c7c7
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-10-09 22:58:36 -0700

    finish mvp styles

--- client/src/App.js
+++ client/src/App.js
@@ -24,25 +24,18 @@ class App extends Component {
           <div className="container max-width flex justify-between items-center">
             <div className="site-logo">
               <a href="/" className="custom-logo-link">
-                Brand Icons
+                <h1>Brand Icons</h1>
               </a>
             </div>
-            <div className="menu-primary-container flex items-center justify-center">
-              <form>
-                <label>
-                  <input type="text" name="name" placeholder="Search" />
-                </label>
-                <input type="submit" value="Submit" />
-              </form>
-            </div>
+            <div className="menu-primary-container flex items-center justify-center"></div>
             <div className="flex items-center">
               <nav id="share" class="social-nav nav flex items-center justify-end">
                 <div className="menu-social-container">
 
                   <ul className="social-menu header-font medium smooth gray h5 list-reset">
 
-                    <li><a href="https://github.com/rdimascio/brandicons" target="_blank" rel="noopener noreferrer"><img src="http://localhost:3001/github" width="22" alt="View us on GitHub" /></a></li>
-                    <li><a href="https://twitter.com/brandicons" target="_blank" rel="noopener noreferrer"><img src="http://localhost:3001/twitter" width="22" alt="Say Hi on Twitter" /></a></li>
+                    <li><a href="https://github.com/rdimascio/brand-icons" target="_blank" rel="noopener noreferrer"><img src="http://localhost:3001/github" width="22" alt="View us on GitHub" /></a></li>
+                    <li><a href="https://twitter.com/intent/follow?screen_name=brand_icons" target="_blank" rel="noopener noreferrer"><img src="http://localhost:3001/twitter" width="22" alt="Say Hi on Twitter" /></a></li>
 
                   </ul>
 
@@ -51,23 +44,30 @@ class App extends Component {
             </div>
           </div>
         </header>
-          <div className="grid justify-start items-center">
-            {
-              this.state.icons && <ul className="brand__images brand__images_grid">
-                {this.state.icons.map(icon => (
-                  <li key={icon.title}>
-                    <a className="brand__images_image" href={`http://localhost:3001/${icon.path}`}>
-                      <img src={`http://localhost:3001/${icon.path}`} alt={icon.title}/>
-                    </a>
-                    <div className="brand__images_info">
-                      <h2>{icon.title}</h2>
-                      <p>#{icon.tags}</p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            }
-          </div>
+        <div className="flex justify-center items-center text-center hero_section">
+          <form>
+            <h1>A Curated Collection of Brand Icons</h1>
+            <p>Download or link to any brand SVG for free. An open-source project maintained by <a href="https://twitter.com/intent/follow?screen_name=dimasc_io"  target="_blank" rel="noopener noreferrer">@dimasc_io</a></p>
+            <input type="text" name="name" placeholder="Search Brands" />
+          </form>
+        </div>
+        <div className="grid justify-start items-center">
+          {
+            this.state.icons && <ul className="brand__images brand__images_grid">
+              {this.state.icons.map(icon => (
+                <li key={icon.title}>
+                  <a className="brand__images_image" href={`http://localhost:3001/${icon.path}`}>
+                    <img src={`http://localhost:3001/${icon.path}`} alt={icon.title}/>
+                  </a>
+                  <div className="brand__images_info">
+                    <h2>{icon.title}</h2>
+                    <p>#{icon.tags}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          }
+        </div>
         <footer className="flex justify-center">
           <div className="text-center flex justify-center items-center subscibe__form">
             <div id="revue-embed">
