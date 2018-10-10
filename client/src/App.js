fb2888d46961e3affa01c79ee0c164338d0f38f9
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-10-09 21:11:20 -0700

    add subsciption form to footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -20,9 +20,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <div className="navbar">
+        <header className="site-header drop-in">
+          <div className="container max-width flex justify-between items-center">
+            <div className="site-logo">
+              <a href="/" className="custom-logo-link">
+                Brand Icons
+              </a>
+            </div>
+            <div className="menu-primary-container flex items-center justify-center">
+              <form>
+                <label>
+                  <input type="text" name="name" placeholder="Search" />
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+            <div className="flex items-center">
+              <nav id="share" class="social-nav nav flex items-center justify-end">
+                <div className="menu-social-container">
 
+                  <ul className="social-menu header-font medium smooth gray h5 list-reset">
+
+                    <li><a href="https://github.com/rdimascio/brandicons" target="_blank" rel="noopener noreferrer"><img src="http://localhost:3001/github" width="22" alt="View us on GitHub" /></a></li>
+                    <li><a href="https://twitter.com/brandicons" target="_blank" rel="noopener noreferrer"><img src="http://localhost:3001/twitter" width="22" alt="Say Hi on Twitter" /></a></li>
+
+                  </ul>
+
+                </div>
+              </nav>
+            </div>
           </div>
         </header>
           <div className="grid justify-start items-center">
@@ -42,9 +68,19 @@ class App extends Component {
               </ul>
             }
           </div>
-        <footer>
-          <div className="">
-            
+        <footer className="flex justify-center">
+          <div className="text-center flex justify-center items-center subscibe__form">
+            <div id="revue-embed">
+              <h1 className="subscribe__header">Get new icons in your inbox</h1>
+              <form action="https://www.getrevue.co/profile/brandicons/add_subscriber" method="post" id="revue-form" name="revue-form" >
+              <div className="revue-form-group">
+                <input className="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email" />
+              </div>
+              <div className="revue-form-actions">
+                <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit" />
+              </div>
+              </form>
+            </div>
           </div>
         </footer>
       </div>
