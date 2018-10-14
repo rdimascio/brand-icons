0b4646167fcd1b70a76fcfee63893fc9e3ee699d
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-10-13 18:57:54 -0700

    fix headers sent

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.headersSent('Access-Control-Allow-Origin', '*')
+  res.headersSent('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
