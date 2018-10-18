148a7e77c76a466e1eab1ad60c50d8962ba81790
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-10-17 20:35:22 -0700

    update frontend paths

--- client/src/App.js
+++ client/src/App.js
@@ -294,8 +294,8 @@ class App extends Component {
             {_icons.map(l => {
               return (
                 <div className="brand__card">
-                  <a className='brand__images_image' href={`https://cdn.brandicons.org/icons/${l.path}.svg`}>
-                    <div style={{ backgroundImage: `url(https://cdn.brandicons.org/icons/${l.path}.svg)`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '4rem'}}></div>
+                  <a className='brand__images_image' href={`https://brandicons.org/${l.path}`}>
+                    <div style={{ backgroundImage: `url(https://brandicons.org/${l.path})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '4rem'}}></div>
                   </a>
                   <div className='brand__images_info'>
                     <p className='brand__title'>{l.title}</p>
