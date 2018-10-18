73308f53d53eaeab295faf2c600b4dca1fec8ea4
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-10-17 20:27:25 -0700

    update route for light and dark icons

--- routes/single.js
+++ routes/single.js
@@ -2,12 +2,12 @@ const data = require('../data/brands.json')
 const path = require('path')
 
 module.exports = (req, res) => {
-  const reqpath = req.params.path
+  const reqpath = req.originalUrl
   // const brand = data.brands.find(m => m.path === reqpath)
 
   // Not using the brand path, just using what was passed in for now
   res.setHeader('Content-Type', 'image/svg+xml')
   // res.sendFile(path.join(__dirname, '../icons/', reqpath + '.svg'))
-  res.status(301).redirect('https://cdn.brandicons.org/icons/' + reqpath + '.svg')
+  res.status(301).redirect('https://cdn.brandicons.org/icons' + reqpath + '.svg')
   // res.status(200).json({ brand });
 }
